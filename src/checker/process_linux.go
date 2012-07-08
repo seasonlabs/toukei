8d026ba3e1248dcedfe566435c2959ff415de369
Victor Castell <victorcoder@gmail.com>
2012-07-08 22:21:54 +0200

    Watch heads in linux

--- src/checker/process_linux.go
+++ src/checker/process_linux.go
@@ -11,12 +11,29 @@ func Check(path string) {
 	if err != nil {
 		log.Fatal(err)
 	}
-	
-	err = watcher.Watch(path)
+
+	file, err := os.Open(path)
 	if err != nil {
 		log.Fatal(err)
 	}
 
+	fis, err := file.Readdir(0)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	ch = make(chan Stat)
+
+	for _, fi := range fis {
+		if fi.IsDir() {
+			err = watcher.Watch(path + "/" + fi.Name() + "/refs/heads/master")
+			if err != nil {
+				log.Fatal(err)
+			}
+		}
+	}
+	
+	
 	for {
 		select {
 		case ev := <-watcher.Event:
